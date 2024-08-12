3e1489006c93c55b0aeb308611534c7d6b2fbc39
Topu-Roy <topuroy143333@gmail.com>
2024-08-13 01:35:10 +0600

    Added comment

--- src/lib/cn.tsx
+++ src/lib/cn.tsx
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// * Utility function for conditional tailwindCSS classNames
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
